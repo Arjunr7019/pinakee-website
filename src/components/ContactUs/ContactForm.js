ecb847259660d0d8e636abb85871931c9a8e674a
arjun r <arjun.rdell@gmail.com>
2024-10-19 21:24:44 +0530

    now website is responsive

--- src/components/ContactUs/ContactForm.js
+++ src/components/ContactUs/ContactForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Contact.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import instagram from '../../img/bxl-instagram.svg';
 import linkedIn from '../../img/bxl-linkedin.svg';
@@ -6,8 +7,8 @@ import twitter from '../../img/bxl-twitter.svg';
 
 export default function ContactForm() {
     return (
-        <div className='mx-5 py-5 d-flex justify-content-around align-items-center flex-row' style={{height:"80vh"}}>
-            <div className='w-50 d-flex justify-content-center align-items-center flex-column'>
+        <div className='ContactForm mx-5 py-5 d-flex justify-content-around align-items-center flex-column flex-sm-row'>
+            <div className='ContactForm-OurContacts mb-4 mb-sm-0 d-flex justify-content-center align-items-start align-items-sm-center flex-column'>
                 <h2 className='w-50 fw-bold mb-3'>Our Contacts</h2>
                 <p className='w-50 mb-5' style={{ lineHeight: "180%" }}>Bengaluru,India <br />+91-86608 87277 <br />hello@pinakeedigitech.com</p>
                 <p className='w-50 m-0'>Follow us:</p>
@@ -17,12 +18,12 @@ export default function ContactForm() {
                     <img className='mx-1' style={{ fill: "#F95A1E", cursor:"pointer" }} src={twitter} alt="icon" />
                 </div>
             </div>
-            <div className='w-50'>
+            <div className='ContactForm-OurContacts'>
                 <form className='w-100 d-flex justify-content-center align-items-center flex-column'>
-                    <input style={{ border: "2px solid #E5E5E5" }} className='w-50 rounded-5 px-3 py-2 mb-3' type="text" name="name" id="name" placeholder='Name' />
-                    <input style={{ border: "2px solid #E5E5E5" }} className='w-50 rounded-5 px-3 py-2 mb-3' type="text" name="email" id="email" placeholder='Email' />
-                    <textarea style={{ border: "2px solid #E5E5E5", resize: "none" }} className='w-50 rounded-5 px-3 py-2 mb-3' name="comment" id="comment" rows="6" placeholder='Type your message here..' ></textarea>
-                    <div className='w-50 d-flex justify-content-end align-items-center'>
+                    <input style={{ border: "2px solid #E5E5E5" }} className='formInputs rounded-5 px-3 py-2 mb-3' type="text" name="name" id="name" placeholder='Name' />
+                    <input style={{ border: "2px solid #E5E5E5" }} className='formInputs rounded-5 px-3 py-2 mb-3' type="text" name="email" id="email" placeholder='Email' />
+                    <textarea style={{ border: "2px solid #E5E5E5", resize: "none" }} className='formInputs rounded-5 px-3 py-2 mb-3' name="comment" id="comment" rows="6" placeholder='Type your message here..' ></textarea>
+                    <div className='formInputs d-flex justify-content-end align-items-center'>
                         <button className='px-4 py-2 border-0 rounded-pill text-light' style={{
                             background: "linear-gradient(0deg, rgba(249,90,30,1) 0%, rgba(255,149,32,1) 100%)"
                         }}>Submit</button>
--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Contact.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Icon1 from '../../img/icon1.svg';
 import Icon2 from '../../img/icon2.svg';
@@ -8,7 +9,7 @@ export default function ContactUs() {
     return (
         <div className="py-5" style={{backgroundColor:"#F0F0F0"}}>
             <h1 className="fw-bold text-center mb-4">Contact us</h1>
-            <div className="d-flex justify-content-center  flex-row">
+            <div className="d-flex justify-content-center align-items-center flex-column flex-sm-row">
                 <div className="w-25 d-flex justify-content-center flex-column">
                     <img className="m-auto" style={{height:"50%"}} src={Icon1} alt="icon" />
                     <h5 className="fw-bold text-center" style={{ color: "#F95A1E" }}>Contacts</h5>
